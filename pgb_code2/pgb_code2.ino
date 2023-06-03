e0fae3a7a788b403cd9a971a2b62b4ad4e3e86de
SarD-CM <A01652414@tec.mx>
2023-06-02 22:07:37 -0600

    arduino funciona

--- pgb_code2/pgb_code2.ino
+++ pgb_code2/pgb_code2.ino
@@ -1,53 +1,74 @@
-
 #include <DueTimer.h>
+#include <math.h>
 
 //el santiago no hizo nada
-//initial vairables
-
+float revolutions = 0;
 float temporal = 0;
+long startTime = 0;
+long elapsedTime;
+float mrpm;
 float wrpm;
-double vel;
+float vel;
+float temp;
 int potenciometer;
 
-float rpm = 0;
-unsigned long timeold;
+int capacity = 52;
+float dischargeTime = 0.0;
 
-double pi = 3.1415926536;
-double wm = 0.0;
+long pi = 3.1415926536;
+long wm = 0.0;
 float kv = 8.9460;
-float torque = 0.0;
+
+double torque = 0.0;
+
 float pmec = 0.0;
 float pmot = 0.0;
 float pbatt = 0.0;
 
-volatile int ISRCounter = 0;
+const int timeThreshold = 5;
+long timeCounter = 0;
+const int maxSteps = 255;
+float ISRCounter = 0;
+unsigned long timeold;
 int counter = 0;
 long deltaTime = 0;
+bool IsCW = true;
 
-int p = 0;
-int im = 0;
-int ib = 0;
-int vm = 0;
-int vb = 0;
+float throttle = 0.0;
 
-int imF = 0;
-int ibF = 0.0;
-int vmF = 0.0;
-int vbF = 0.0;
-int wmF = 0.0;
+float p = 0.0;
+float im = 0.0;
+float ib = 0.0;
+float vm = 0.0;
+float vb = 0.0;
+
+float imF = 0.0;
+float ibF = 0.0;
+float vmF = 0.0;
+float vbF = 0.0;
+float wmF = 0.0;
 
 double imF2 = 0.0;
+double ibF2 = 0.0;
 double vmF2 = 0.0;
+double vbF2 = 0.0;
+double wmF2 = 0.0;
 
-double currentm = 0.0;
-double currentb = 0.0;
-double voltagem = 0.0;
-double voltageb = 0.0;
+int soc = 0.0;
+int dischargeF = 0.0;
 
-//pin assigments
+float currentm = 0.0;
+float currentb = 0.0;
+float voltagem = 0.0;
+float voltageb = 0.0;
+
+
 #define channelPinA 2
 #define channelPinB 3
 #define throttlePedalOut 9
+#define pedalSwitch 4
+#define switchOut 9
+#define switchIn 6
 
 #define throttlePedalIn A0
 #define currentSensor A1
@@ -56,183 +77,125 @@ double voltageb = 0.0;
 #define currentBat A4
 
 //Filter assignments
+#define WINDOW_SIZE 10
+#define WINDOW_S 25
+#define WINDOW_S2 5
 
-#define WINDOW_SIZE 5
+int INDEX1, INDEX2, INDEX3, INDEX4, INDEX5, INDEX6 = 0;
+int SUM1, SUM2, SUM3, SUM4, SUM5, SUM6 = 0;
+int READINGS1[WINDOW_SIZE], READINGS2[WINDOW_S], READINGS3[WINDOW_SIZE], READINGS4[WINDOW_SIZE], READINGS5[WINDOW_SIZE], READINGS6[WINDOW_S2];
+float result1, result2, result3, result4, result5, result6;
 
-int INDEX = 0;
-int SUM = 0;
-int READINGS[WINDOW_SIZE];
+//Function assignments
+void motorSensorRead();
+void batterySensorRead();
+void doEncode();
+void throttleFun();
 
-int INDEX1 = 0;
-int SUM1 = 0;
-int READINGS1[WINDOW_SIZE];
-
-int INDEX2 = 0;
-int SUM2 = 0;
-int READINGS2[WINDOW_SIZE];
-
-int INDEX3 = 0;
-int SUM3 = 0;
-int READINGS3[WINDOW_SIZE];
-
-int INDEX4 = 0;
-int SUM4 = 0;
-int READINGS4[WINDOW_SIZE];
-
-int INDEX5 = 0;
-int SUM5 = 0;
-int READINGS5[WINDOW_SIZE];
+float doFilter1();
+float doFilter2();
+float doFilter3();
+float doFilter4();
+float doFilter5();
+float doFilter6();
 
 void setup() {
-
   analogReadResolution(12);
   Serial.begin(115200);
+  pinMode(channelPinA, INPUT_PULLUP);  // set pin to input
+  pinMode(channelPinB, INPUT_PULLUP);
+  pinMode(switchIn, INPUT_PULLUP);
+  pinMode(pedalSwitch, INPUT_PULLUP);
+  pinMode(switchOut, OUTPUT);
 
-  //Input for encoders
-  pinMode(channelPinA, INPUT_PULLUP);  // First signal for encoder
-  pinMode(channelPinB, INPUT_PULLUP);  // Second signal for encoder
+  pinMode(5,OUTPUT);
 
-  //timed analog reads
-  Timer3.attachInterrupt(batterySensorRead).setFrequency(200).start();
-  Timer5.attachInterrupt(motorSensorRead).setFrequency(200).start();
-  Timer4.attachInterrupt(throttle).setFrequency(200).start();
+  Timer3.attachInterrupt(batterySensorRead).setFrequency(500).start();
+  Timer4.attachInterrupt(throttleFun).setFrequency(200).start();
+  Timer5.attachInterrupt(motorSensorRead).setFrequency(500).start();
 
   attachInterrupt(digitalPinToInterrupt(channelPinA), doEncode, RISING);
-
 }
 
 void loop() {
-  Serial.println(" "); //This could be avoided but apparently \n scares santi
-
-  //Second axle RPM calculation
-
-  if (millis() - timeold >= 5) {
+  digitalWrite(5,HIGH);
+  if (millis() - timeold >= 200) {
 
     temporal = ISRCounter * 60000; //(millis()-timeold);
-    wm = float(temporal / ((millis() - timeold) * 96.0)); // aqui esta
-    //Serial.println(wm);
+    wm = float(temporal / ((millis() - timeold) * 96.0)); // aqui esta |
     timeold = millis();
     ISRCounter = 0;
   }
-  /*
-    SUM1 = SUM1 - READINGS1[INDEX1]; // Remove the oldest entry from the sum
-    READINGS1[INDEX1] = wm; // Add the newest reading to the window
-    SUM1 = SUM1 + wm; // Add the newest reading to the sum
-    INDEX1 = (INDEX1 + 1) % WINDOW_SIZE; // Increment the index, and wrap to 0 if it exceeds the window size
-    wmF = SUM1 / (WINDOW_SIZE); // Divide the sum of the window by the window size for the result
-  */
 
-  wmF = Filter(wm);
   delay(500);
-  String outRPM1 = String("Motor velocity en[rpm]: ") + wmF;
+  //Filtering
+  wmF = doFilter1(wm);
+  vmF = doFilter2(vm);
+  imF = doFilter3(im);
+  ibF = doFilter4(ib);
+  vbF = doFilter5(vb);
+  //Calculation
 
-  Serial.print(vmF);
-  //Serial.print(",");
-  //Serial.println(wm);
-
-  //Motor RPM
-  rpm = voltagem * kv;
-  String outRPM2 = String("Motor velocity vm[rpm]: ") + rpm;
-  //Serial.println(outRPM2);
-  //Signal aquisition and filtering
-  //Motor Voltage
-
-  vmF2 = vmF * 0.0008056641;
-  voltagem = -20.571 * voltagem + 55.76; // assigns the voltage value to the adc lecture
-  String outVOLM = String("Motor voltage      [V]: ") + voltagem;
-
-  //Serial.println(outVOLM);
-
-  SUM2 = SUM2 - READINGS2[INDEX2]; // Remove the oldest entry from the sum
-  READINGS2[INDEX2] = vm; // Add the newest reading to the window
-  SUM2 = SUM2 + vm; // Add the newest reading to the sum
-  INDEX2 = (INDEX2 + 1) % WINDOW_SIZE; // Increment the index, and wrap to 0 if it exceeds the window size
-  vmF = SUM2 / WINDOW_SIZE; // Divide the sum of the window by the window size for the result
-
-  //Battery Voltage
-  voltageb = map(vb, 0, 3353, 0, 52.8);   //same
-  String outVOLB = String("Battery voltage    [V]: ") + voltageb;
+  wrpm = wmF * pi / (30 / 1.24137);
+  vel = wrpm * 0.14;
+  mrpm=wm*1.5;
 
 
-  SUM3 = SUM3 - READINGS3[INDEX3]; // Remove the oldest entry from the sum
-  READINGS3[INDEX3] = vb; // Add the newest reading to the window
-  SUM3 = SUM3 + vb; // Add the newest reading to the sum
-  INDEX3 = (INDEX3 + 1) % WINDOW_SIZE; // Increment the index, and wrap to 0 if it exceeds the window size
-  vbF = SUM3 / WINDOW_SIZE; // Divide the sum of the window by the window size for the result
 
-  //Motor Current
-  imF2 = imF * 0.0008056641;    //same
-  currentm = 0.0042 * imF2 + 2.624;           //BUT it converts the voltage to the corresponding current
+  vbF2 = vbF * 0.0008056641 + 0.01;
+  voltageb = vbF2 / 0.055521879;
 
-  String outIM = String("Motor current      [A]: ") + currentm;
-
-
-  SUM4 = SUM4 - READINGS4[INDEX4]; // Remove the oldest entry from the sum
-  READINGS4[INDEX4] = im; // Add the newest reading to the window
-  SUM4 = SUM4 + im; // Add the newest reading to the sum
-  INDEX4 = (INDEX4 + 1) % WINDOW_SIZE; // Increment the index, and wrap to 0 if it exceeds the window size
-  imF = SUM4 / WINDOW_SIZE; // Divide the sum of the window by the window size for the result
-
-
-  //Battery current
-  float ibF2 = map(ib, 0, 4096, 0, 3.3);    //same
+  ibF2 = ibF * 0.0008056641 + 0.01;
   currentb = 0.0042 * ibF2 + 2.624;
 
-  String outIB = String("Battery current    [A]: ") + currentb;
+  vmF2 = vmF * 0.0008056641 + 0.01;
+  voltagem = voltageb - vmF2 / 0.0543259557;
+
+  imF2 = imF * 0.0008056641 + 0.01;
+  currentm = 0.0042 * imF2 + 2.624;
+
+  torque = currentm * 0.0981 + 0.2081;
+
+  pmec = torque * wmF;
+  pmot = voltagem * currentm;
+  pbatt = voltageb * currentb;
+
+  //SoC
+
+  soc = 0.5314 * pow(voltageb, 3) - 80.77 * pow(voltageb, 2) + 4101.7 * voltageb - 69512;
+  if (soc > 100) {
+    soc = 100;
+  }
+  else
+  {
+    soc = soc;
+  }
 
 
-  SUM5 = SUM5 - READINGS5[INDEX5]; // Remove the oldest entry from the sum
-  READINGS5[INDEX5] = ib; // Add the newest reading to the window
-  SUM5 = SUM5 + ib; // Add the newest reading to the sum
-  INDEX5 = (INDEX5 + 1) % WINDOW_SIZE; // Increment the index, and wrap to 0 if it exceeds the window size
-  ibF = SUM5 / WINDOW_SIZE; // Divide the sum of the window by the window size for the result
+  //Discharge time
 
-
-  //Motor torque
-  torque = 0.0981 * currentm + 0.2081;      // using kt*Im we use the current to calculate the motor mechanical torque
-  String outT = String("Motor torque      [Nm]: ") + torque;
-
-  //Pedal
+  dischargeTime = capacity * soc / (currentb * 100);
+  dischargeF = doFilter6(dischargeTime);
 
   potenciometer = map(p, 0, 4096, 0, 254);  // read pot vealue and map it for a PWM scale
-  float throttle = map(potenciometer, 0, 253, 0, 100); // sabe but not for a 0 to 100% scale
+  throttle = map(potenciometer, 0, 253, 0, 100); // sabe but not for a 0 to 100% scale
   analogWrite(throttlePedalOut, potenciometer);  // output PWM
-  String outA = String("Throttle           [%]: ") + throttle;
 
-  //Power
-  pmot = voltagem * currentm;      // P = V*I
-  pbatt = voltageb * currentb;     // ... same
-  wrpm = (wm / 60) * (2 * pi);     // angular velocity in rad/s
-  pmec = (wrpm * pi / 30) * torque ;  //rpm turned into rad/s
-  vel = wrpm * (0.14);              // wheel diameter 28cm, linear velocity wr
+  //Serial.println(dischargeTime);
+  //Serial.println(dischargeF);
+  //String list[] = { String(voltageb), String(voltagem), String(currentm), String(currentb), String(torque), String(throttle), String(wm), String(vel), String(pmec), String(pmot), String(pbatt), String(soc), String(dischargeF)};
 
-  String outP1 = String("M Power             [W]: ") + pmot;
-  String outP2 = String("B Power             [W]: ") + pbatt;
-  String outP3 = String("Mec Power           [W]: ") + pmec;
+  //Serial.print(list[2] + "," + list[3] + "," + list[1] + "," + list[4] + "," + list[7] + "," + list[6] + "," + list[5] + "," + list[8] + "," + list[9] + "," + list[10] + ',' + list[11] + ',' + list[12] + ',' + list[13]);
 
-
-  //Print section
-  /*
-    Serial.println(outRPM1);
-    Serial.println(outVOLM);
-    Serial.println(outVOLB);
-    Serial.println(outIM);
-    Serial.println(outIB);
-    Serial.println(outA);
-    Serial.println(outT);
-    Serial.println(outP1);
-    Serial.println(outP2);
-    Serial.println(outP3);
-  */
-
-  //With this list we send the data packet to the raspberry pi
-  String list[] = { String(voltageb), String(voltagem), String(currentm), String(currentb), String(torque), String(throttle), String(wm), String(vel), String(pmec), String(pmot), String(pbatt)};
-  // list[11] invokes the ancient ones so please do not use it
-  //Serial.println("VM  ,IM  , VB   ,IB  ,RPM ,TH ,TQ  ,VEL ,PMC ,PMT ,PBT ");
-  //Serial.print(list[2]+","+list[3]+","+list[1]+","+list[4]+","+list[7]+","+list[6]+","+list[5]+","+list[8]+","+list[9]+","+list[10]+','+ String(pbatt));  //Do this instead
+  Serial.println(String (voltagem) + ',' + String(currentm) + ',' + String(voltageb) + ',' + String(currentb) + ',' + String(mrpm) + ',' + String(throttle) + ',' + String(torque) + ',' + String(vel) + ',' + String(pmec) + ',' + String(pmot) + ',' + String(pbatt) + ',' + String(soc) + ',' + String(dischargeF));
 }
 
-//Timed reads
+
+void doEncode()
+{
+  ISRCounter++;
+}
+
 void motorSensorRead() {
   im = analogRead(currentSensor);
   vm = analogRead(voltageSensor);
@@ -243,24 +206,72 @@ void batterySensorRead() {
   ib = analogRead(currentBat);
 }
 
-void throttle() {
+void throttleFun() {
   p = analogRead(throttlePedalIn);
 }
 
-//Count rpm
-void doEncode()
-{
 
-  ISRCounter++;
+float doFilter1(int x) {
 
+  SUM1 = SUM1 - READINGS1[INDEX1]; // Remove the oldest entry from the sum
+  READINGS1[INDEX1] = x; // Add the newest reading to the window
+  SUM1 = SUM1 + x; // Add the newest reading to the sum
+  INDEX1 = (INDEX1 + 1) % WINDOW_SIZE; // Increment the index, and wrap to 0 if it exceeds the window size
+  result1 = SUM1 / WINDOW_SIZE; // Divide the sum of the window by the window size for the result
+
+  return result1;
 }
 
-int Filter(int x) {
-  SUM = SUM - READINGS[INDEX]; // Remove the oldest entry from the sum
-  READINGS[INDEX] = x; // Add the newest reading to the window
-  SUM = SUM + x; // Add the newest reading to the sum
-  INDEX = (INDEX + 1) % WINDOW_SIZE; // Increment the index, and wrap to 0 if it exceeds the window size
-  int result = SUM / WINDOW_SIZE; // Divide the sum of the window by the window size for the result
-  
-  return result;
+float doFilter2(int x) {
+
+  SUM2 = SUM2 - READINGS2[INDEX2]; // Remove the oldest entry from the sum
+  READINGS2[INDEX2] = x; // Add the newest reading to the window
+  SUM2 = SUM2 + x; // Add the newest reading to the sum
+  INDEX2 = (INDEX2 + 1) % WINDOW_S; // Increment the index, and wrap to 0 if it exceeds the window size
+  result2 = SUM2 / WINDOW_S; // Divide the sum of the window by the window size for the result
+
+  return result2;
+}
+
+float doFilter3(int x) {
+
+  SUM3 = SUM3 - READINGS3[INDEX3]; // Remove the oldest entry from the sum
+  READINGS3[INDEX3] = x; // Add the newest reading to the window
+  SUM3 = SUM3 + x; // Add the newest reading to the sum
+  INDEX3 = (INDEX3 + 1) % WINDOW_SIZE; // Increment the index, and wrap to 0 if it exceeds the window size
+  result3 = SUM3 / WINDOW_SIZE; // Divide the sum of the window by the window size for the result
+
+  return result3;
+}
+float doFilter4(int x) {
+
+  SUM4 = SUM4 - READINGS4[INDEX4]; // Remove the oldest entry from the sum
+  READINGS4[INDEX4] = x; // Add the newest reading to the window
+  SUM4 = SUM4 + x; // Add the newest reading to the sum
+  INDEX4 = (INDEX4 + 1) % WINDOW_SIZE; // Increment the index, and wrap to 0 if it exceeds the window size
+  result4 = SUM4 / WINDOW_SIZE; // Divide the sum of the window by the window size for the result
+
+  return result4;
+}
+
+float doFilter5(int x) {
+
+  SUM5 = SUM5 - READINGS5[INDEX5]; // Remove the oldest entry from the sum
+  READINGS5[INDEX5] = x; // Add the newest reading to the window
+  SUM5 = SUM5 + x; // Add the newest reading to the sum
+  INDEX5 = (INDEX5 + 1) % WINDOW_SIZE; // Increment the index, and wrap to 0 if it exceeds the window size
+  result5 = SUM5 / WINDOW_SIZE; // Divide the sum of the window by the window size for the result
+
+  return result5;
+}
+
+float doFilter6(int x) {
+
+  SUM6 = SUM6 - READINGS6[INDEX6]; // Remove the oldest entry from the sum
+  READINGS6[INDEX6] = x; // Add the newest reading to the window
+  SUM6 = SUM6 + x; // Add the newest reading to the sum
+  INDEX6 = (INDEX6 + 1) % WINDOW_S2; // Increment the index, and wrap to 0 if it exceeds the window size
+  result6 = SUM6 / WINDOW_S2; // Divide the sum of the window by the window size for the result
+
+  return result6;
 }
