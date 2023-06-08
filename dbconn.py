b3d0df1ca2dc3e838ed2a53ed5822c05ff002d6d
SantiG2233 <67402678+SantiG2233@users.noreply.github.com>
2023-06-07 18:16:38 -0600

    Update dbconn.py

--- dbconn.py
+++ dbconn.py
@@ -3,22 +3,42 @@ import pandas as pd
 from sqlalchemy import create_engine
 from datetime import datetime
 
+Gled = 7
+Yled = 11
+Oled = 13
+Rled = 15
 data_list = []
 
 engine = create_engine('postgresql://postgres:(CIMB2023)@proyectosanti.postgres.database.azure.com:5432/postgres')
 print('Connected to database')
 
-ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
-ser.flush
+ser = serial.Serial('/dev/ttyACM4', 115200, timeout=1)
+ser.flush()
+ser.reset_input_buffer()
+ser.reset_output_buffer()
 
 while True:
-    data = ser.readline().decode().strip() 
-    if data != '':
-        data_split = data.split(",")
-        data_split.insert(0, datetime.now())
-        data_list.append(data_split)
-        df = pd.DataFrame(data_list,columns=['measurement_time','V_mot','I_mot','V_batt','I_batt','RPM','Throttle','Torque','Speed','P_mech','P_mot','P_batt','SOC','Discharge_time'])
-        df.to_sql('measurements', engine, if_exists='append', index=False)
-        data_list = []
-        print('Data UPLOADED')
-        print(df)
+    try:
+        data = ser.readline().decode('utf-8').strip()
+        now = datetime.now()
+        cond = 0
+        print('serial read')
+        print(now)
+        if data != '':
+            data_split = data.split(",")
+            data_split.insert(0,cond)
+            data_split.insert(0, now)
+            data_list.append(data_split)
+            print(len(data_list[0]))
+            if len(data_list[0]) == 15:
+                df = pd.DataFrame(data_list,columns=['measurement_time','Condition','V_mot','I_mot','V_batt','I_batt','rpm','Throttle','Torque','Speed','P_mech','P_mot','P_batt','SOC','Discharge_time'])
+                df.to_sql('measurements', engine, if_exists='append', index=False)
+                print(df)
+                print('Data UPLOADED\n')
+                data_list = []
+            else:
+                print('No furula\n')
+    except Exception as error:
+        print(error)
+    except KeyboardInterrupt:
+        ser.close()
